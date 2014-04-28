c01155b6f4fb21e58e14d9a5138f86b38bdac7a8
Joe Ilagan <joeilagan@Joes-MacBook-Pro-2.local>
2014-04-28 14:57:04 +0800

    Initial Commit

---
+++ TableViewChallenge/JViewController.h
@@ -0,0 +1,13 @@
+//
+//  JViewController.h
+//  TableViewChallenge
+//
+//  Created by Joe Ilagan on 4/28/14.
+//  Copyright (c) 2014 Void Awk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JViewController : UIViewController
+
+@end
---
+++ TableViewChallenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViewChallenge
+//
+//  Created by Joe Ilagan on 4/28/14.
+//  Copyright (c) 2014 Void Awk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JAppDelegate class]));
+    }
+}
